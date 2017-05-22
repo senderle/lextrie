154ae0264dd82d35e0e59d145a9aeaf323fa05eb
Scott Enderle <scott.enderle@gmail.com>
2017-05-22 17:17:51 -0400

    change data root folder

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'lextrie': ['lextrie/_lexica/bing.json'],
+        'lextrie': ['_lexica/*.json'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
